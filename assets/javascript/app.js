48530cc4e09f389a11859173a544edc70813e810
Zachary <zpyingling9559@gmail.com>
2020-01-22 16:45:45 -0500

    adding html document data and css styling

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+// Need 5 linked arrays. One for questions, one for correct answers. and 3 for incorrect answers.
+// Functions needed: loadQuestion(), startTimer(), correctAnswer(), incorrectAnswer(), timesUp() gameOver();
+
+$(document).ready(function() {
+  const questions = [
+    "What does CPU stand for?",
+    "What part in a computer allows connections to all other components of a computer?",
+    "What year was the first computer invented?",
+    "In terms of memory (kilobyte, megabyte, etc), what comes after terabyte",
+    "What isn't an advantage of USB 3.0 compared to USB 2.0?"
+  ];
+
+  const correctAnswer = [
+    "Central Processing Unit",
+    "Motherboard",
+    "1968",
+    "Petabyte",
+    "USB 3.0 technology can't find you happiness"
+  ];
+
+  const incorrectAnswer1 = [
+    "Computer Powering Unit",
+    "Power Supply",
+    "1952",
+    "Gigabyte",
+    "USB 3.0 has faster data transfer speeds"
+  ];
+
+  const incorrectAnswer2 = [
+    "Computer Pushing Unit",
+    "CPU",
+    "1973",
+    "Bit",
+    "USB 3.0 are backwards compatible"
+  ];
+  const incorrectAnswer3 = [
+    "Central Powering Unit",
+    "Hard Drive",
+    "1948",
+    "Exabyte",
+    "USB 3.0 can allow more power to the port"
+  ];
+
+  function startTimer() {}
+
+  function correctAnswer() {}
+
+  function incorrectAnswer() {}
+
+  function timesUp() {}
+
+  function gameOver() {}
+
+  function loadQuestion() {}
+});
